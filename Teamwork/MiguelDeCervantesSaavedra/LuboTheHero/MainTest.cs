1eb060d50dfaecdcf298a674a84bbe0ff6a83c38
Oci Cekov <shortyoko@gmail.com>
2014-02-21 14:33:13 +0200

    Created some classes, just to see how fu***,.... what time do we have.

--- Teamwork/MiguelDeCervantesSaavedra/LuboTheHero/MainTest.cs
+++ Teamwork/MiguelDeCervantesSaavedra/LuboTheHero/MainTest.cs
@@ -1,5 +1,6 @@
 ﻿namespace LuboTheHero
 {
+    using System.Collections.Generic;
     using System;
 
     class MainTest
@@ -8,6 +9,8 @@
         {
             Console.WriteLine( "Test");
             Console.WriteLine(" And now the simple change");
+
+            var list = new List<int> {2, 3, 6, 4};
             Console.WriteLine("lkjhg");
         }
     }
