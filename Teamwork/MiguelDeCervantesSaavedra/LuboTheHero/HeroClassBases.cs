752505f054c37fdff65052b029b9c3527af2ccb3
svilenbonev <svilenbonev@users.noreply.github.com>
2014-02-21 12:36:56 +0200

    created some initial class structure

---
+++ Teamwork/MiguelDeCervantesSaavedra/LuboTheHero/HeroClassBases.cs
@@ -0,0 +1,11 @@
+﻿namespace LuboTheHero
+{
+    public abstract class HeroClassBases
+    {
+        public abstract uint baseHealth { get; protected set; }
+
+        public abstract uint baseMana { get; protected set; }
+
+        public abstract uint baseDamage { get; protected set; }
+    }
+}
